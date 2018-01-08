0c31cbcbd587558998db530b97a531a0bd3fc9d5
yojiwatanabe <yojiwata@gmail.com>
2018-01-08 14:10:45 -0500

    Fixed bug in calculating variability in week answer for multiple weeks

--- AnswerClasses.py
+++ AnswerClasses.py
@@ -1,5 +1,5 @@
 from people 	import *
-from random 	import randint
+from random		import randint, seed
 
 DINNERS_PER_WEEK	= 4
 POSSIBLE_TIMES		= 5 
@@ -7,9 +7,10 @@ DINNER_DAYS			= ["Monday", "Tuesday", "Wednesday", "Thursday"]
 DINNER_TIMES		= ["6:00", "6:30", "7:00", "7:30", "8:00"]
 
 class WeekAnswer():
-	dayAnswers 	= []
-	guests 		= []
-	fitness 	= 0.0
+	dayAnswers 		= []
+	guests 			= []
+	guestAttendance = []
+	fitness 		= 0.0
 
 	def __init__(self):
 		self.guests = People("data.dinner")
@@ -23,6 +24,7 @@ class WeekAnswer():
 			self.dayAnswers.append(answer)
 
 	def createSolution(self, people, day_id):
+		# seed(a=)
 		dinnerTime	= randint(0, POSSIBLE_TIMES - 1)
 		answer 		= DayAnswer(dinnerTime)
 		answer.setFitness(people, day_id)
@@ -34,25 +36,24 @@ class WeekAnswer():
 		self.fitness 	= self.fitness / DINNERS_PER_WEEK
 
 	def variabilityFitness(self):
-		variability = []
-		for guest in self.guests.people:
-			variability.append(0)
-			
+		self.guestAttendance = [0] * self.guests.numPeople
+		for j in range(self.guests.numPeople):
 			for i in range(DINNERS_PER_WEEK):
-				if self.dayAnswers[i].idList[guest.personId] == 1:
-					variability[guest.personId] += 1
+				if self.dayAnswers[i].idList[self.guests.people[j].personId] == 1:
+					self.guestAttendance[self.guests.people[j].personId] += 1
 		
-		attDiff 		= max(variability) - min(variability)
+		attDiff 		= max(self.guestAttendance) - min(self.guestAttendance)
 		if 	 attDiff == 0 : var = 1
 		elif attDiff == 1 : var = 0.995
 		else: 	var 	= ((1.0/(attDiff * attDiff)) / 4) + 0.75
 		
-		print("%.3f * (%.3f / 2)" % (self.fitness, var))
-		print "pre_fit = " + str(self.fitness),
-		self.fitness 	= self.fitness * var
-		print "\tpost fit = " + str(self.fitness)
+		# print("%.3f * (%.3f / 2)" % (self.fitness, var))
+		# print "pre_fit = " + str(self.fitness),
+		print self.guestAttendance
+		self.fitness 			= self.fitness * var
+		# print "\tpost fit = " + str(self.fitness)
 
-		print("attDiff = %i\t and var = %.3f" % (attDiff , var))
+		# print("attDiff = %i\t and var = %.3f" % (attDiff , var))
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 # Answer Object
 # Takes in a full line from a people file, gets information and sets it
